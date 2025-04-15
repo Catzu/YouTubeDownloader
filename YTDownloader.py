64a7cb47de751b68f61acb5c2d39dceea8fc8c7e
Catzu <nt7nguyen@gmail.com>
2025-04-15 13:48:49 +0200

    finish
    finishing touches, renamed program.
    
    pip install tkinter
    pip install customtkinter
    pip uninstall pytube
    pip install pytubefix
    
    pip installer pyinstaller
    pyinstaller --onefile --windowed YTDownloader.py

---
+++ YTDownloader.py
@@ -0,0 +1,53 @@
+import tkinter
+import customtkinter
+import os
+from pytubefix import YouTube
+
+# video folder maker
+os.makedirs("videos", exist_ok=True)
+
+def startDownload():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink)
+        video = ytObject.streams.get_highest_resolution()
+
+        title.configure(text=ytObject.title, text_color="white")
+        finishLabel.configure(text="")
+
+        video.download(output_path="videos")
+    except:
+        finishLabel.configure(text="Download Error", text_color="red")
+        print("Youtube link is invalid")
+    else:
+        finishLabel.configure(text="Downloaded!", text_color="green")
+        print("Download Complete!")
+
+# System settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# App frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Youtube Downloader")
+
+# Adding UI Elements
+title = customtkinter.CTkLabel(app, text="Insert a youtube link")
+title.pack(padx=10, pady=10)
+
+# Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+# Link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link.pack()
+
+# Download button
+download = customtkinter.CTkButton(app, text="Download", command=startDownload)
+download.pack(padx=10, pady=10)
+
+# Run app
+app.mainloop()
